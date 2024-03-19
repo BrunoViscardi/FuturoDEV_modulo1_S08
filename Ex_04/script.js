cb6974f45fc06d89b3d9b32ab5f3d207df7aa054
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-03-18 21:53:16 -0300

    first commit

---
+++ Ex_04/script.js
@@ -0,0 +1,8 @@
+//obtendo o elemento pai
+let span = document.getElementById("unicoSpan")
+//obtendo o elemento filho
+let subtitulo = document.getElementById("subtitulo")
+
+
+//removendo o elemento filho
+span.removeChild(subtitulo)
