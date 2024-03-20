990363130a65f5a02792236f2be1bb09b33610e7
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-03-20 11:52:40 -0300

    first commit

---
+++ Ex_08/script.js
@@ -0,0 +1,8 @@
+//selecionando um elemento html por classe
+let botao = document.getElementsByClassName("botao")[0]
+
+//atribuindo o atributo disabled ao botão
+botao.setAttribute("disabled", "disabled")
+
+
+
