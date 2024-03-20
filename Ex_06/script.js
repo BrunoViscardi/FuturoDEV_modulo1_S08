28d06c68840bbcc60d6b41547ba023356271c452
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-03-20 11:35:28 -0300

    first commit

---
+++ Ex_06/script.js
@@ -0,0 +1,8 @@
+//selecionando um elemento html por tag name ao primeiro que encontrar
+let titulo = document.getElementsByTagName("h1")[0]
+
+//adiciona uma classe (.titulo) ao elemento
+titulo.setAttribute("class", "titulo");
+
+
+
