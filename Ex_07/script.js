c291fbcedc1d980722e42746478b1903a5b0f569
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-03-20 11:40:16 -0300

    first commit

---
+++ Ex_07/script.js
@@ -0,0 +1,8 @@
+//selecionando um elemento html por tag name ao primeiro que encontrar
+let titulo = document.getElementsByTagName("h1")[0]
+
+//remove a classe (.titulo) do elemento
+titulo.removeAttribute("class");
+
+
+
